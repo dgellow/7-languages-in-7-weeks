d4071d4c5c6ad0d495a352e0ff51dcfb774280ee
dgellow <samuel.elborai@gmail.com>
2013-10-17 21:43:22 +0200

    Add my solution for the 2nd day 3nd task

--- ruby/homeworks.rb
+++ ruby/homeworks.rb
@@ -50,3 +50,16 @@ family_tree.visit_all {|node| puts node.node_name}
 #    uncle
 #    child 3
 #    child 4
+
+
+## Task 3
+# Write a simple grep that will print the lines of a file having any
+# occurrences of a phrase anywhere in that line. You will need to do
+# a simple regular expression match and read lines from a file. (This
+# is surprisingly simple in Ruby.) If you want, include line numbers.
+
+def my_grep(filename, text)
+  File.new("tree.rb").each_line do |line|
+    puts line if line =~ /#{text}/
+  end
+end

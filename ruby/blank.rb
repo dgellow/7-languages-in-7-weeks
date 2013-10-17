21a019b7af873e05a4a6e11dd878a9fcbdfde233
dgellow <samuel.elborai@gmail.com>
2013-10-17 21:51:38 +0200

    Demonstrate the way to extend existing classes

---
+++ ruby/blank.rb
@@ -0,0 +1,19 @@
+#!/bin/env ruby
+
+# Demonstrate how to modify an existing class to add behavior
+class NilClass
+  def blank?
+    true
+  end
+end
+
+class String
+  def blank?
+    self.size == 0
+  end
+end
+
+["", "person", nil].each do |element|
+  puts element unless element.blank?
+end
+# => 'person'

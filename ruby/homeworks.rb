51c447405b6c56aebf15cb7ea0c091aa355c710d
dgellow <samuel.elborai@gmail.com>
2013-10-17 21:30:53 +0200

    Add the description of the 2nd task

--- ruby/homeworks.rb
+++ ruby/homeworks.rb
@@ -17,6 +17,12 @@ end
 (1..16).each_slice(4).each {|arr| puts arr.join}
 
 ## Task 2
+# The Tree class was interesting, but it did not allow you to specify
+# a new tree with a clean user interface. Let the initializer accept a
+# nested structure with hashes and arrays. You should be able to
+# specify a tree like this:
+# {'grandpa' => {'dad' => {'child 1' => {}, 'child 2' => {} }, 'uncle' => {'child 3' => {}, 'child 4' => {} } } }
+
 class Tree
   attr_accessor :children, :node_name
 

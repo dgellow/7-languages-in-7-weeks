13fb14044ec434febc484ea3f5b35da4e1463b93
dgellow <samuel.elborai@gmail.com>
2013-10-17 21:28:51 +0200

    Add my solution for the 2nd day 2nd task

--- ruby/homeworks.rb
+++ ruby/homeworks.rb
@@ -15,3 +15,33 @@ end
 
 ##Now, do the same with `each_slice' in 'Enumerable`.
 (1..16).each_slice(4).each {|arr| puts arr.join}
+
+## Task 2
+class Tree
+  attr_accessor :children, :node_name
+
+  def initialize(name, children={})
+    @node_name = name
+    @children = children.map {|key, value| Tree.new(key, value)}
+  end
+
+  def visit_all(&block)
+    visit &block
+    children.each {|c| c.visit_all &block}
+  end
+
+  def visit(&block)
+    block.call self
+  end
+end
+
+family_tree = Tree.new("Family", {'grandpa' => {'dad' => {'child 1' => {}, 'child 2' => {} }, 'uncle' => {'child 3' => {}, 'child 4' => {} } } })
+family_tree.visit_all {|node| puts node.node_name}
+# => Family
+#    grandpa
+#    dad
+#    child 1
+#    child 2
+#    uncle
+#    child 3
+#    child 4
